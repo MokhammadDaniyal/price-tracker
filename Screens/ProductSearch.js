f047ae246e86aaa29f1e62708e854a0e89ac34dc
Daniyal <mokhammad.daniyal@gmail.com>
2020-06-29 03:43:19 -0700

    Added clear Icon to Input

--- Screens/ProductSearch.js
+++ Screens/ProductSearch.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import {StyleSheet, View, Text, Button} from 'react-native';
 
+import Input from '../Components/Input';
+
 function ProductSearchScreen(props) {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+      }}>
+      <Input placeholder="Product Name" />
+
       <Text>ProductSearch!</Text>
       <Button
         onPress={() => {
