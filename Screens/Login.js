3769307461e5152a1fb7d14dafb3c8bd38bedb17
Vlad Magay <wiko.kz@yandex.kz>
2020-06-28 19:40:57 -0700

    added BEAUTIFUL login screen

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,29 +1,274 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  TextInput,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import {Button} from '@ant-design/react-native';
 import {connect} from 'react-redux';
-
+import Animated, {Easing} from 'react-native-reanimated';
+import {TapGestureHandler, State} from 'react-native-gesture-handler';
 import {loginUser} from '../store/userReducer/actions';
+import Svg, {Image, Circle, ClipPath} from 'react-native-svg';
+function runTiming(clock, value, dest) {
+  const state = {
+    finished: new Value(0),
+    position: new Value(0),
+    time: new Value(0),
+    frameTime: new Value(0),
+  };
+
+  const config = {
+    duration: 1000,
+    toValue: new Value(0),
+    easing: Easing.inOut(Easing.ease),
+  };
+
+  return block([
+    cond(clockRunning(clock), 0, [
+      set(state.finished, 0),
+      set(state.time, 0),
+      set(state.position, value),
+      set(state.frameTime, 0),
+      set(config.toValue, dest),
+      startClock(clock),
+    ]),
+    timing(clock, state, config),
+    cond(state.finished, debug('stop clock', stopClock(clock))),
+    state.position,
+  ]);
+}
+
+const {width, height} = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: 'white',
+    height: 70,
+    marginHorizontal: 20,
+    borderRadius: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 5,
+    shadowOffset: {width: 2, height: 2},
+    shadowColor: 'black',
+    shadowOpacity: 0.2,
+  },
+  textInput: {
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 0.5,
+    marginHorizontal: 20,
+    paddingLeft: 10,
+    marginVertical: 5,
+    borderColor: 'rgba(0,0,0,0.2)',
+    backgroundColor: 'white',
+    color: 'black',
+  },
+  closeButton: {
+    height: 40,
+    width: 40,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: -20,
+    left: width / 2 - 20,
+    shadowOffset: {width: 2, height: 2},
+    shadowColor: 'black',
+    shadowOpacity: 0.2,
+  },
+});
+
+const {
+  Value,
+  event,
+  block,
+  cond,
+  eq,
+  set,
+  Clock,
+  startClock,
+  stopClock,
+  debug,
+  timing,
+  clockRunning,
+  interpolate,
+  Extrapolate,
+  concat,
+} = Animated;
 
 const LoginScreen = (props) => {
+  let buttonOpacity = new Value(1);
+  const onStateChange = event([
+    {
+      nativeEvent: ({state}) =>
+        block([
+          cond(
+            eq(state, State.END),
+            set(buttonOpacity, runTiming(new Clock(), 1, 0)),
+          ),
+        ]),
+      // block([cond(eq(state, State.END)), set(buttonOpacity, 0)]),
+    },
+  ]);
+  const buttonY = interpolate(buttonOpacity, {
+    inputRange: [0, 1],
+    outputRange: [100, 0],
+    extrapolate: Extrapolate.CLAMP,
+  });
+
+  const bgY = interpolate(buttonOpacity, {
+    inputRange: [0, 1],
+    outputRange: [-height / 3 - 50, 0],
+    extrapolate: Extrapolate.CLAMP,
+  });
+
+  const textInputZindex = interpolate(buttonOpacity, {
+    inputRange: [0, 1],
+    outputRange: [1, -1],
+    extrapolate: Extrapolate.CLAMP,
+  });
+
+  const textInputY = interpolate(buttonOpacity, {
+    inputRange: [0, 1],
+    outputRange: [0, 100],
+    extrapolate: Extrapolate.CLAMP,
+  });
+  const textInputOpacity = interpolate(buttonOpacity, {
+    inputRange: [0, 1],
+    outputRange: [1, 0],
+    extrapolate: Extrapolate.CLAMP,
+  });
+  const rotateCross = interpolate(buttonOpacity, {
+    inputRange: [0, 1],
+    outputRange: [180, 360],
+    extrapolate: Extrapolate.CLAMP,
+  });
+  const onCloseState = event([
+    {
+      nativeEvent: ({state}) =>
+        block([
+          cond(
+            eq(state, State.END),
+            set(buttonOpacity, runTiming(new Clock(), 0, 1)),
+          ),
+        ]),
+      // block([cond(eq(state, State.END)), set(buttonOpacity, 0)]),
+    },
+  ]);
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Button
-        style={{marginBottom: 10, width: 150}}
-        size="large"
-        type="primary"
-        onPress={() => props.navigation.navigate('Auth')}>
-        Login
-      </Button>
-      <Text style={{fontSize: 18}}>or</Text>
-      <Button
-        style={{marginTop: 10, width: 150}}
-        size="large"
-        type="primary"
-        onPress={() => props.navigation.navigate('Signup')}>
-        Sign up
-      </Button>
-    </View>
+    <KeyboardAvoidingView
+      style={{flex: 1, justifyContent: 'center'}}
+      behavior="padding">
+      <View
+        style={{flex: 1, backgroundColor: 'white', justifyContent: 'flex-end'}}>
+        <Animated.View
+          style={{
+            ...StyleSheet.absoluteFill,
+            transform: [{translateY: bgY}],
+            zIndex: 2,
+          }}>
+          <Svg height={height + 50} width={width}>
+            <ClipPath id="clip">
+              <Circle r={height + 50} cx={width / 2} />
+            </ClipPath>
+            <Image
+              href={require('../assets/bg.jpg')}
+              width={width}
+              height={height + 50}
+              preserveAspectRatio="xMidYMid slice"
+              clipPath="url(#clip)"
+            />
+          </Svg>
+        </Animated.View>
+        <View style={{height: height / 3, justifyContent: 'center'}}>
+          <TapGestureHandler onHandlerStateChange={onStateChange}>
+            <Animated.View
+              style={{
+                ...styles.button,
+                opacity: buttonOpacity,
+                transform: [{translateY: buttonY}],
+              }}>
+              <Text style={{fontSize: 20, fontWeight: 'bold'}}>SIGN IN</Text>
+            </Animated.View>
+          </TapGestureHandler>
+          <Animated.View
+            style={{
+              ...styles.button,
+              backgroundColor: '#2E71DC',
+              opacity: buttonOpacity,
+              transform: [{translateY: buttonY}],
+            }}>
+            <Text style={{fontSize: 20, fontWeight: 'bold'}}>SIGN UP</Text>
+          </Animated.View>
+          <Animated.View
+            style={{
+              zIndex: textInputZindex,
+              opacity: textInputOpacity,
+              transform: [{translateY: textInputY}],
+              height: height / 3,
+              ...StyleSheet.absoluteFill,
+              top: null,
+              justifyContent: 'center',
+            }}>
+            <TapGestureHandler onHandlerStateChange={onCloseState}>
+              <Animated.View style={styles.closeButton}>
+                <TouchableWithoutFeedback
+                  onPress={Keyboard.dismiss}
+                  accessible={false}>
+                  <Animated.Text
+                    style={{transform: [{rotate: concat(rotateCross, 'deg')}]}}>
+                    X
+                  </Animated.Text>
+                </TouchableWithoutFeedback>
+              </Animated.View>
+            </TapGestureHandler>
+            <TextInput
+              placeholder="EMAIL"
+              style={styles.textInput}
+              placeholderTextColor="black"
+            />
+            <TextInput
+              placeholder="PASSWORD"
+              style={styles.textInput}
+              placeholderTextColor="black"
+            />
+            <Animated.View style={styles.button}>
+              <Text style={{fontSize: 20, fontWeight: 'bold'}}>SIGN IN</Text>
+            </Animated.View>
+          </Animated.View>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+    // <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    //   <Button
+    //     style={{marginBottom: 10, width: 150}}
+    //     size="large"
+    //     type="primary"
+    //     onPress={() => props.navigation.navigate('Auth')}>
+    //     Login
+    //   </Button>
+    //   <Text style={{fontSize: 18}}>or</Text>
+    //   <Button
+    //     style={{marginTop: 10, width: 150}}
+    //     size="large"
+    //     type="primary"
+    //     onPress={() => props.navigation.navigate('Signup')}>
+    //     Sign up
+    //   </Button>
+    // </View>
   );
 };
 
