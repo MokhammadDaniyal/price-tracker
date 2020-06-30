3c894adc90b56950bf92b22af46bcdaf42b27f1d
Daniyal <mokhammad.daniyal@gmail.com>
2020-06-29 17:10:49 -0700

    Added Picker library

--- Screens/ProductSearch.js
+++ Screens/ProductSearch.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View, Text, Button} from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 import Input from '../Components/Input';
 
 function ProductSearchScreen(props) {
+  const [website, setWebsite] = useState(null);
   return (
     <View
       style={{
@@ -11,6 +13,36 @@ function ProductSearchScreen(props) {
         alignItems: 'center',
         justifyContent: 'flex-start',
       }}>
+      <DropDownPicker
+        placeholder="Select a website"
+        items={[
+          {label: 'Amazon', value: 'amazon'},
+          {label: 'Newegg', value: 'newegg'},
+        ]}
+        containerStyle={{
+          height: 40,
+          flexDirection: 'row',
+          marginHorizontal: 20,
+          height: 50,
+          marginVertical: 5,
+        }}
+        style={{
+          borderTopLeftRadius: 25,
+          borderTopRightRadius: 25,
+          borderBottomLeftRadius: 25,
+          borderBottomRightRadius: 25,
+          borderWidth: 0.4,
+          borderColor: 'black',
+        }}
+        dropDownStyle={{
+          borderWidth: 0.4,
+          borderColor: 'black',
+          borderBottomLeftRadius: 25,
+          borderBottomRightRadius: 25,
+        }}
+        defaultValue={website}
+        onChangeItem={(item) => setWebsite(item.value)}
+      />
       <Input placeholder="Product Name" />
 
       <Text>ProductSearch!</Text>
