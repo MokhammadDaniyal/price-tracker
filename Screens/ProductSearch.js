4a2d67422bd36372b19d753b2e0a73b39a246131
Daniyal <mokhammad.daniyal@gmail.com>
2020-06-30 02:12:24 -0700

    adde flat list to product search

--- Screens/ProductSearch.js
+++ Screens/ProductSearch.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import {StyleSheet, View, Text, Button, FlatList, Image} from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 
+import images from '../images';
 import Input from '../Components/Input';
 
 function ProductSearchScreen(props) {
@@ -44,7 +45,9 @@ function ProductSearchScreen(props) {
         onChangeItem={(item) => setWebsite(item.value)}
       />
       <Input placeholder="Product Name" />
-
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <FlatList data={DATA} renderItem={({item}) => renderListItem(item)} />
+      </View>
       <Text>ProductSearch!</Text>
       <Button
         onPress={() => {
@@ -54,5 +57,60 @@ function ProductSearchScreen(props) {
     </View>
   );
 }
+
+function renderListItem({title, price, reviews}) {
+  return (
+    <View
+      style={{
+        borderColor: 'black',
+        borderWidth: 0.2,
+        backgroundColor: '#D3D3D3',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginHorizontal: 10,
+        height: 200,
+        borderRadius: 10,
+        marginVertical: 5,
+      }}>
+      <Image
+        style={{flex: 1, height: 150, marginLeft: 20}}
+        source={images.ps4}
+      />
+      <View
+        style={{
+          marginLeft: 25,
+          flex: 1.5,
+          height: '100%',
+          borderColor: 'red',
+          borderWidth: 1,
+          justifyContent: 'space-evenly',
+        }}>
+        <Text>{title}</Text>
+        <Text>Price: {price}</Text>
+        <Text>{reviews} Reviews</Text>
+      </View>
+    </View>
+  );
+}
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+    price: 2500,
+    reviews: 1231123,
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+    price: 2500,
+    reviews: 1231123,
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+    price: 2500,
+    reviews: 1231123,
+  },
+];
 
 export default ProductSearchScreen;
